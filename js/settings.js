255068e6aa1049d1cebacfe9b9228a99539a616b
midota-phucdaivo <phucvd@midota.com>
2021-05-20 01:11:30 +0700

    init page

---
+++ js/settings.js
@@ -0,0 +1,12 @@
+// Init global namespace
+window.Panthera = {};
+
+// On non-proudction environments, there is more output on the consolve
+window.Panthera.IS_PRODUCTION = true;
+
+// API endpoint where the CMS is accesible.
+window.Panthera.API_ENDPOINT = '/cms';
+
+// Assets path, change if served through cloudfront CDN
+window.Panthera.ASSET_PATH = 'http://localhost:3004/';
+
